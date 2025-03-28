e44655b35c6423f5afbc1f1067d1dba9f3415b95
SK <164775232+klymsk@users.noreply.github.com>
2025-03-28 22:24:21 +0200

    mainFiles

---
+++ App.js
@@ -0,0 +1,15 @@
+import Header from "./components/header";
+import Main from "./components/main";
+import "./styles/styles.css"
+
+export default function App() {
+  return (
+    <body>
+      <Header />
+
+      <Main />
+    </body>
+  );
+}
+
+
